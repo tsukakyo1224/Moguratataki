550185c802ec8efb7a62de16e7a07e7818632867
tsukakyo1224 <45038683+tsukakyo1224@users.noreply.github.com>
2019-07-02 17:41:40 +0900

    vr

---
+++ TGS/Assets/Scripts/CameraCol.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCol : MonoBehaviour
+{
+
+    public GameObject targetObj;
+    public Vector3 targetPos;
+
+
+
+    void Start()
+    {
+
+        targetObj = GameObject.Find("Main Camera");
+        targetPos = targetObj.transform.position;
+    }
+
+    void Update()
+    {
+        
+        // targetの移動量分、自分（カメラ）も移動する
+        targetPos = targetObj.transform.position;
+        transform.position += targetObj.transform.position - targetPos;
+        //矢印を押したぶんカメラを回す
+        transform.RotateAround(targetPos, Vector3.up, (Input.GetAxis("Horizontal")) * Time.deltaTime * 200f);
+        
+
+    }
+}
