b9d0f22c23769d0a092331758ecfe649fb045f27
tsukakyo1224 <45038683+tsukakyo1224@users.noreply.github.com>
2019-07-18 17:58:57 +0900

    sa

--- TGS/Assets/Scripts/ControllerCol.cs
+++ TGS/Assets/Scripts/ControllerCol.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ControllerCol : MonoBehaviour
 {
+    private PhotonView MyPhotonView;
+    private PhotonTransformView photonTransformView;
+
     public static GameObject RightContoroller;
     Quaternion angle;
     Vector3 V3_angle;
@@ -12,35 +15,38 @@ public class ControllerCol : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        MyPhotonView = PhotonView.Get(this);
+        photonTransformView = GetComponent<PhotonTransformView>();
         RightContoroller = GameObject.Find("Controller (right)");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (MyPhotonView.isMine)
+        {
+            V3_localAngle = new Vector3(0f, 90f, 0f);
+            //VIVEコントローラーの位置をハンマーに代入
+            this.transform.position = RightContoroller.transform.position;
 
-        V3_localAngle = new Vector3(0f, 90f, 0f);
-        //VIVEコントローラーの位置をハンマーに代入
-        this.transform.position = RightContoroller.transform.position;
 
-        
-        //VIVEコントローラーの角度をハンマーに代入
-        angle = RightContoroller.transform.rotation;
-        //Quaternionをオイラー角Vecter3に変換
-        V3_angle = angle.eulerAngles;
-        /*
-        V3_angle.x += 90f;
-        V3_angle.y += 0f;
-        V3_angle.z += 0f;*/
-        //tmp = V3_angle.x;
-        //V3_angle.x = -V3_angle.z;
-        //V3_angle.z = tmp;
+            //VIVEコントローラーの角度をハンマーに代入
+            angle = RightContoroller.transform.rotation;
+            //Quaternionをオイラー角Vecter3に変換
+            V3_angle = angle.eulerAngles;
+            /*
+            V3_angle.x += 90f;
+            V3_angle.y += 0f;
+            V3_angle.z += 0f;*/
+            //tmp = V3_angle.x;
+            //V3_angle.x = -V3_angle.z;
+            //V3_angle.z = tmp;
 
-        //オイラー角Vecter3をQuaternionに変換(戻す)
-        //angle = Quaternion.AngleAxis(90, -Vector3.forward);
-        angle = Quaternion.Euler(V3_angle);
+            //オイラー角Vecter3をQuaternionに変換(戻す)
+            //angle = Quaternion.AngleAxis(90, -Vector3.forward);
+            angle = Quaternion.Euler(V3_angle);
 
-        this.transform.rotation = angle;
+            this.transform.rotation = angle;
+        }
     }
 }
