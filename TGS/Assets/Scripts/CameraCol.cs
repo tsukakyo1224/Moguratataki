082725b097d0f08d5ea4cff6603824a92b756d2c
tsukakyo1224 <kyon-1224.rioru@ezweb.ne.jp>
2019-07-18 16:46:38 +0900

    aa

--- TGS/Assets/Scripts/CameraCol.cs
+++ TGS/Assets/Scripts/CameraCol.cs
@@ -19,22 +19,25 @@ public class CameraCol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (PhotonNetwork.player.ID == 1)
+        {
+            //VIVEコントローラーの角度をハンマーに代入
+            angle = this.transform.rotation;
+            //Quaternionをオイラー角Vecter3に変換
+            V3_angle = angle.eulerAngles;
+            V3_angle.y = 180f;
 
-        //VIVEコントローラーの角度をハンマーに代入
-        angle = this.transform.rotation;
-        //Quaternionをオイラー角Vecter3に変換
-        V3_angle = angle.eulerAngles;
-        V3_angle.y = 180f;
+            angle = Quaternion.Euler(V3_angle);
 
-        angle = Quaternion.Euler(V3_angle);
-
-        this.transform.rotation = angle;
+            this.transform.rotation = angle;
 
 
-        //if (PhotonNetwork.player.ID == 1)
-        //{
-        this.transform.position = new Vector3(0.0f, 1.0f, 3.5f);
+            //if (PhotonNetwork.player.ID == 1)
+            //{
+            this.transform.position = new Vector3(0.0f, 1.0f, 3.5f);
             //Debug.Log("aa");
-        //}
+            //}
+        }
+
     }
 }
